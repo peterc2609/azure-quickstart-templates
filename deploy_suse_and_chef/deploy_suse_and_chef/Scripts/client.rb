06c8416270e321b9537b44f81acd0d4a56dbc7cd
Ryan Irujo <ryan.irujo@gmail.com>
2015-09-28 14:15:47 +0200

    Update client.rb

--- deploy_suse_and_chef/deploy_suse_and_chef/Scripts/client.rb
+++ deploy_suse_and_chef/deploy_suse_and_chef/Scripts/client.rb
@@ -1,6 +1,6 @@
-log_level				:info
-log_location			STDOUT
-chef_server_url			'https://chefsrv.westeurope.cloudapp.azure.com/organizations/learn_chef/learn_chef_12_env'
+log_level				        :info
+log_location			      STDOUT
+chef_server_url			    'https://chefsrv.westeurope.cloudapp.azure.com/organizations/learn_chef/learn_chef_12_env'
 validation_client_name	'learn_chef_12_env-validator'
-validation_key			'/etc/chef/learn_chef_12_env-validator.pem'
-client_key				'/etc/chef/client.pem'
+validation_key			    '/etc/chef/learn_chef_12_env-validator.pem'
+client_key				      '/etc/chef/client.pem'
