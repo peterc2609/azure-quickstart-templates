298a3b8a912393be2b721010f27c30ae2c429457
Ryan Irujo <ryan.irujo@gmail.com>
2015-09-29 14:15:01 +0200

    updated knife.rb

--- deploy_suse_and_chef/deploy_suse_and_chef/Scripts/knife.rb
+++ deploy_suse_and_chef/deploy_suse_and_chef/Scripts/knife.rb
@@ -1,9 +1,9 @@
 log_level               :info
 log_location            STDOUT
 node_name               'chefadmin'
-client_key              '~/chef-repo/.chef/chefadmin.pem'
+client_key              '/root/chef-repo/.chef/chefadmin.pem'
 validation_client_name  'learn_chef_12_env-validator'
-validation_key          '~/chef-repo/.chef/learn_chef_12_env-validator.pem'
+validation_key          '/root/chef-repo/.chef/learn_chef_12_env-validator.pem'
 chef_server_url         'https://chefsrv.westeurope.cloudapp.azure.com/organizations/learn_chef_12_env'
-syntax_check_cache_path '~/chef-repo/.chef/syntax_check_cache'
-cookbook_path           [ '~/chef-repo/cookbooks']
+syntax_check_cache_path '/root/chef-repo/.chef/syntax_check_cache'
+cookbook_path           [ '/root/chef-repo/cookbooks']
