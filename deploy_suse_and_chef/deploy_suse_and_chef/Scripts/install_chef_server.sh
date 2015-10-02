a5f327a60946c97d3949aa55c50c876545989240
Ryan Irujo <ryan.irujo@gmail.com>
2015-10-02 18:01:36 +0200

    updated install_chef_server.sh script

--- deploy_suse_and_chef/deploy_suse_and_chef/Scripts/install_chef_server.sh
+++ deploy_suse_and_chef/deploy_suse_and_chef/Scripts/install_chef_server.sh
@@ -10,10 +10,11 @@
 # - Creates the first Chef Organization on the Server
 #
 #
-# Syntax: ./install_chef_server.sh -u CHEF_USERNAME -p CHEF_PASSWORD
+# Syntax:  ./install_chef_server.sh -u CHEF_USERNAME -p CHEF_PASSWORD -i CHEF_IP -r CHEF_REGION -h CHEF_HOSTNAME
+# Example: ./install_chef_server.sh -u chefadmin -p P@ssw0rd! -i 10.0.1.44 -r 'West Europe' -h chefsrv001
 #
 # Parse Script Parameters
-while getopts ":u:p:" opt; do
+while getopts ":u:p:i:r:h:" opt; do
   case "${opt}" in
         u) # Chef Admin Username
              CHEF_USERNAME=${OPTARG}
@@ -21,27 +22,86 @@ while getopts ":u:p:" opt; do
         p) # Chef Admin Password
              CHEF_PASSWORD=${OPTARG}
              ;;
+        i) # Chef Server IP Address
+             CHEF_IP=${OPTARG}
+             ;;
+        r) # Chef Server Azure Region
+             CHEF_REGION=${OPTARG}
+             ;;
+        h) # Chef Server Hostname
+             CHEF_HOSTNAME=${OPTARG}
+             ;;
         \?) # Unrecognised option - show help
-              echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed. -u CHEF_USERNAME and -p CHEF_PASSWORD are the only options available."
-                exit 2
-                ;;
+            echo -e \\n"Option [-${BOLD}$OPTARG${NORM}] is not allowed. All Valid Options are listed below:"
+            echo -e "-u CHEF_USERNAME - Username of the Chef Server Administrator."
+            echo -e "-p CHEF_PASSWORD - Password of the Chef Server Administrator."
+            echo -e "-i CHEF_IP       - IP Address of the Chef Server."
+            echo -e "-r CHEF_REGION   - Region where the Chef Server is being deployed in Azure, i.e. - East US, West US, West Europe, etc."
+            echo -e "-h CHEF_HOSTNAME - Hostname of the Chef Server."\\n
+            echo -e "An Example of how to use this script is shown below:"
+            echo -e "./install_chef_server.sh -u chefadmin -p P@ssw0rd1! -i 10.0.1.44 -r 'West Europe' -h chefsrv001"\\n
+            exit 2
+            ;;
   esac
 done
 shift $((OPTIND-1))
 
 # Verifying the Script Parameters Values exist.
 if [ -z "${CHEF_USERNAME}" ]; then
-    echo "Chef Username must be set."
+    echo "Chef Server Username must be provided."
     exit 2
 fi
 
 if [ -z "${CHEF_PASSWORD}" ]; then
-    echo "Chef Password must be set."
+    echo "Chef Server Password must be provided."
     exit 2
 fi
 
+if [ -z "${CHEF_IP}" ]; then
+    echo "Chef Server IP Address must be provided."
+    exit 2
+fi
+
+if [ -z "${CHEF_REGION}" ]; then
+    echo "Chef Server Azure Region must be provided."
+    exit 2
+fi
+
+if [ -z "${CHEF_HOSTNAME}" ]; then
+    echo "Chef Server Hostname must be provided."
+    exit 2
+fi
+
+# Parsing out the Chef Azure Region values and modifying the value accordingly.
+
+if [ "$CHEF_REGION" == "West Europe" ]; then
+    echo "changing 'West Europe' to 'westeurope'"
+    AZURE_REGION="westeurope"
+fi
+
+if [ "$CHEF_REGION" == "East US" ]; then
+    echo "changing 'East US' to 'eastus'"
+    AZURE_REGION="eastus"
+fi
+
+if [ "$CHEF_REGION" == "West US" ]; then
+    echo "changing 'West US' to 'westus'"
+    AZURE_REGION="westus"
+fi
+
+if [ "$CHEF_REGION" == "East Asia" ]; then
+    echo "changing 'East Asia' to 'eastasia'"
+    AZURE_REGION="eastasia"
+fi
+
+if [ "$CHEF_REGION" == "South East Asia" ]; then
+    echo "changing 'South East Asia' to 'seasia'"
+    AZURE_REGION="seasia"
+fi
+
 # Updating the Chef Server Hosts File
-sudo sed -i "2i10.0.1.4 chefsrv.westeurope.cloudapp.azure.com CHEFSRV" /etc/hosts
+sed -i "2i$CHEF_IP $CHEF_HOSTNAME\\.$AZURE_REGION\\.cloudapp.azure.com $CHEF_HOSTNAME" /etc/hosts
+#sudo sed -i "2i10.0.1.4 chefsrv.westeurope.cloudapp.azure.com CHEFSRV" /etc/hosts
 
 # Printing out the correct FQDN of the Server
 hostname -f
