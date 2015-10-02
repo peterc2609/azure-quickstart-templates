d2b546f69868f4a584ef3a201b20382bd2940af2
Ryan Irujo <ryan.irujo@gmail.com>
2015-10-03 00:22:51 +0200

    updated install_chef-client.sh Script

--- deploy_suse_and_chef/deploy_suse_and_chef/Scripts/install_chef-client.sh
+++ deploy_suse_and_chef/deploy_suse_and_chef/Scripts/install_chef-client.sh
@@ -1,10 +1,88 @@
 #!/bin/bash
+#
 # install_chef-client.sh
-# Script is designed to run as 'root'
-# USE THIS SCRIPT AT YOUR OWN RISK!
+#
+# This Script is responsible for performing the following tasks on a Suse Server:
+# - Install the Chef Client 12.4.3 (RPM)
+# - Install the ChefDK 0.8.0 (RPM)
+#
+# Syntax:  ./install_chef-client.sh -u SUSE_USERNAME -p SUSE_PASSWORD -i SUSE_IP -r SUSE_REGION -h SUSE_HOSTNAME -c CHEF_HOSTNAME
+# Example: ./install_chef-client.sh -u suseadmin -p P@ssw0rd! -i 10.0.1.44 -r westeurope -h susesrv001 -c chefsrv001
+#
+# The following scripts need to be parametrized and modified:
+# - retrieve-chef-client-validator-cert.exp
+# - retrieve-chefadmin-user-cert.exp
+#
+# Parse Script Parameters
+while getopts ":u:p:i:r:h:c:" opt; do
+  case "${opt}" in
+        u) # Suse Admin Username
+             SUSE_USERNAME=${OPTARG}
+             ;;
+        p) # Suse Admin Password
+             SUSE_PASSWORD=${OPTARG}
+             ;;
+        i) # Suse Server IP Address
+             SUSE_IP=${OPTARG}
+             ;;
+        r) # Suse Server Azure Region
+             SUSE_REGION=${OPTARG}
+             ;;
+        h) # Suse Server Hostname
+             SUSE_HOSTNAME=${OPTARG}
+             ;;
+        c) # Chef Server Hostname
+             CHEF_HOSTNAME=${OPTARG}
+             ;;			 
+        \?) # Unrecognised option - show help
+            echo -e \\n"Option [-${BOLD}$OPTARG${NORM}] is not allowed. All Valid Options are listed below:"
+            echo -e "-u SUSE_USERNAME - Username of the Suse Server Administrator."
+            echo -e "-p SUSE_PASSWORD - Password of the Suse Server Administrator."
+            echo -e "-i SUSE_IP       - IP Address of the Suse Server."
+            echo -e "-r SUSE_REGION   - Region where the Suse Server is being deployed in Azure."
+            echo -e "-h SUSE_HOSTNAME - Hostname of the Suse Server."
+			echo -e "-h CHEF_HOSTNAME - Hostname of the Suse Server."\\n
+            echo -e "An Example of how to use this script is shown below:"
+            echo -e "./install_chef-client.sh -u suseadmin -p P@ssw0rd1! -i 10.0.1.44 -r westeurope -h susesrv001"\\n
+            exit 2
+            ;;
+  esac
+done
+shift $((OPTIND-1))
+
+# Verifying the Script Parameters Values exist.
+if [ -z "${SUSE_USERNAME}" ]; then
+    echo "Suse Server Username must be provided."
+    exit 2
+fi
+
+if [ -z "${SUSE_PASSWORD}" ]; then
+    echo "Suse Server Password must be provided."
+    exit 2
+fi
+
+if [ -z "${SUSE_IP}" ]; then
+    echo "Suse Server IP Address must be provided."
+    exit 2
+fi
+
+if [ -z "${SUSE_REGION}" ]; then
+    echo "Suse Server Azure Region must be provided."
+    exit 2
+fi
+
+if [ -z "${SUSE_HOSTNAME}" ]; then
+    echo "Suse Server Hostname must be provided."
+    exit 2
+fi
+
+if [ -z "${CHEF_HOSTNAME}" ]; then
+    echo "Chef Server Hostname must be provided."
+    exit 2
+fi
 
 # Updating the Chef Server Hosts File
-sudo sed -i "2i10.0.1.10 sles12srv.westeurope.cloudapp.azure.com SLES12SRV" /etc/hosts
+sudo sed -i "2i$SUSE_IP $SUSE_HOSTNAME\\.$SUSE_REGION\\.cloudapp.azure.com $SUSE_HOSTNAME" /etc/hosts
 
 # Printing out the correct FQDN of the Server
 hostname -f
@@ -23,7 +101,7 @@ sudo rpm -Uvh chef-12.4.3-1.el6.x86_64.rpm
 mkdir -p /etc/chef/trusted_certs
 
 # Retrieving the Chef Server Certificate
-knife ssl fetch https://chefsrv.westeurope.cloudapp.azure.com
+knife ssl fetch https://$CHEF_HOSTNAME\.$SUSE_REGION\.cloudapp.azure.com
 
 # Copying the Chef Server SSL to '/etc/chef/trusted_certs'
 cp /.chef/trusted_certs/* /etc/chef/trusted_certs/
@@ -65,8 +143,3 @@ wget https://raw.githubusercontent.com/starkfell/azure-quickstart-templates/mast
 # Running expect script to retrieve chefadmin user Certificate from the Chef Server
 cd /Downloads/
 /usr/bin/expect retrieve-chefadmin-user-cert.exp
-
-# Retrieving the Chef Server SSL Certificate
-cd /root/chef-repo/
-knife ssl fetch https://chefsrv.westeurope.cloudapp.azure.com/
-
